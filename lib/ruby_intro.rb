add2413b2b3b02bc954282f92df1b6da2be43074
mehdigorjian <mehdi.gorjian@gmail.com>
2021-02-04 18:31:16 +0000

    homework 1 parts 1,2,3 tested with rspec

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,56 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.sort.last(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # arr.product(arr).any? {|couple| sum(couple) == n}
+  return false if arr.empty? or arr.length == 1
+	h = Hash.new
+	arr.each{|x| 
+		if h.key? x
+			return true
+		else
+			h[n - x] = n	
+		end
+	}
+	return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  %Q(Hello, #{name})
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+	attr_accessor :price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Illegal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
